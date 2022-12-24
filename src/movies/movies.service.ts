0fd5fd0d0806a85acc10a9099086840f26840c4e
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-12-24 18:30:11 +0900

    nestJS REST API

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { NotFoundException } from '@nestjs/common/exceptions';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -33,7 +34,7 @@ export class MoviesService {
     });
   }
 
-  update(id: number, updateData: CreateMovieDto) {
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updateData });
